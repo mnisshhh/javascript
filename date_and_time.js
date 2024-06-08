6556075afa1b62a331af296f480c701ba06d0730
mnisshhh <mnisshhh1@gmail.com>
2024-06-08 18:33:12 +0000

    date and time

---
+++ date_and_time.js
@@ -0,0 +1,24 @@
+//// dates
+
+const tdDates = new Date;
+
+// console.log(tdDates);
+// console.log(tdDates.toString());
+// console.log(tdDates.toJSON());
+// console.log(tdDates.toLocaleDateString()); //dd-mm-yyyy
+// console.log(tdDates.toLocaleString());
+
+// to declare back date
+
+// let newDate = new Date(2023, 1, 23, 17, 3, 25); // 17,3,25 stands for 5:03:25 PM
+
+// console.log(newDate.toLocaleString());
+
+
+let newDate = new Date();
+
+console.log(newDate);
+console.log(newDate.getDate());
+console.log(newDate.getDay());
+console.log(newDate.getFullYear());
+console.log(newDate.getHours());
