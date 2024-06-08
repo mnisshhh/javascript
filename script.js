6f815c253f8afdafba808ae6d64f7859a48e0db7
mnisshhh <mnisshhh1@gmail.com>
2024-06-08 05:53:16 +0000

    multi-divide_function

---
+++ script.js
@@ -0,0 +1,16 @@
+function multiby() {
+    num1 = document.getElementById("first_number").value;
+    num2 = document.getElementById("second_number").value;
+
+     document.getElementById("result").innerHTML = num1 * num2;
+}
+
+///somehow divide function is not working i dont know the reason but will try to fix it soon
+
+function divideby() {
+    num3 = document.getElementById("first_number").value;
+    num4 = document.getElementById("second_number").value;
+
+    document.getElementById("divideres").innerHTML = num1 / mum2;
+
+}
