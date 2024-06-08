7f62c2b131bb7fac1d0fca08542a012c3478fe47
mnisshhh <mnisshhh1@gmail.com>
2024-06-08 17:56:29 +0000

    numbers and math

---
+++ math_and_numbers.js
@@ -0,0 +1,33 @@
+// // const score = 200;
+
+// // console.log(score);
+
+// // const anotherscore = new Number(500);
+// // console.log(anotherscore);
+// // console.log(anotherscore.toString().length);
+// // console.log(anotherscore.toFixed(3));
+
+// const anotherNumber = 1257856;
+
+// //this bring commas b/w numbers and makes easy to read, however this brings values in US format which is literally worst of world
+
+// console.log(anotherNumber.toLocaleString());
+
+// // to get the same for Indian system use this code
+
+// console.log(anotherNumber.toLocaleString('en-IN'));
+
+// --------------------------------------------------------------- Js Math ----------------------------------------------------------------------
+
+// console.log(Math.round(4.4));
+// console.log(Math.round(4.5));
+// console.log(Math.round(4.6));
+// console.log(Math.abs(-5));
+
+// console.log(Math.random()*10)
+// console.log(Math.floor(Math.random(10,20) * 10 ));
+
+// const min = 1;
+// const max = 10;
+
+// console.log(Math.floor(Math.random() * (max - min + 1) + min));
