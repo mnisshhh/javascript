31e26ea13be62c86ab1e265f98361a3347c6ed9f
Manish Kumar Singh <mnisshhh1@gmail.com>
2024-06-09 11:04:42 +0000

    fun_01

---
+++ Functions/functions_1.js
@@ -0,0 +1,16 @@
+function addtwoNumbers(number1, number2) {
+    return number1 + number2
+    // let result = number1 + number2
+    // return result
+}
+
+const result = addtwoNumbers(5, 4)
+
+// console.log("The result is:", result);
+
+function isloggedIn(username) {
+    return `${username} just logged in`
+}
+
+// console.log(isloggedIn('manish'));
+
