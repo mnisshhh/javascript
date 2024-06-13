e944d22f54255bc490868cfedfcd727f13672f12
Manish Kumar Singh <mnisshhh1@gmail.com>
2024-06-13 17:18:49 +0000

    arrows

---
+++ scopes/arrows.js
@@ -0,0 +1,29 @@
+// const user = {
+//     username: 'manish',
+//     age: 21,
+//     welcomemsg: function() {
+//         console.log(`${this.username} , welcome to website`)
+//     }
+// }
+
+// const arrow = () => {
+//     let username = 'manish'
+//     console.log(this);
+// }
+// arrow()
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++++ arrow function +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+const addtwo = (num1, num2) => {
+    return num1 + num2;
+}
+
+// console.log(addtwo(3,4));
+
+// another type to write the same
+
+const addthree = (num3, num4) =>  num3 + num4 
+
+// console.log(addthree(5,8));
+
