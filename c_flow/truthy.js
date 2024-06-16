e08fb98d5a881a052e3b7dc87396efb7e40d56ac
Manish Kumar Singh <mnisshhh1@gmail.com>
2024-06-16 07:06:10 +0000

    change in truthy

--- c_flow/truthy.js
+++ c_flow/truthy.js
@@ -1,9 +1,9 @@
 const username = ['manish']
 
 if  (username) {
-    console.log(`got the username: ${username}`)
+    // console.log(`got the username: ${username}`)
 } else {
-    console.log('dont have username')
+    // console.log('dont have username')
 }
 
 //falsy values
@@ -17,7 +17,7 @@ if  (username) {
 // to check whether a array is empty or not
 
 if (username.length === 0) {
-    console.log("array is empty")
+    // console.log("array is empty")
 } else {
     // console.log(`array has values: ${username}`)
 }
@@ -27,7 +27,24 @@ if (username.length === 0) {
 const myObj = {}
 
 if  (Object.keys(myObj).length === 0) {
-    console.log("object is empty")
+    // console.log("object is empty")
 } else {
-    console.log("object has some values")
+    // console.log("object has some values")
 }
+
+// nullish coalescing operator (??); null undefined
+
+let val1;
+
+// val1 = null ?? 10
+// val1 = 5 ?? 10
+// val1 = undefined ?? 10
+// answer will be 10, this is to safeguard from null value
+// console.log(val1)
+
+// terniary operator
+
+const clothPrice = 100
+
+clothPrice >= 150 ? console.log("more than 150") : console.log("less than 150")
+
