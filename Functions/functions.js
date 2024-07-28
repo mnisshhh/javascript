446836426428dc7dc3d513b9e0f06df6c9d336ed
Manish Kumar Singh <mnisshhh1@gmail.com>
2024-07-28 17:14:43 +0000

    sum using for each method

---
+++ Functions/functions.js
@@ -0,0 +1,6 @@
+function addNumbers(x, y) {
+    return x + y; 
+}
+
+const answer = addNumbers(5,5);
+console.log(answer);
---
+++ for_each/for_each01.js
@@ -0,0 +1,26 @@
+//adding multiple numbes using foreach method
+
+// const numbers = [34, 54, 34, 25];
+// let total = 0;
+
+// numbers.forEach(function myfunc(sum) {
+//     total += sum;
+// });
+
+
+// console.log(total);
+
+// addming multiple numbers using foreach methor and arrow function
+
+const numbers = [25, 85, 35, 54]; // numbes which we want to add 'stored in an array called numbers'
+let sum = 0; // declaring sum equals 0
+
+// numbers.forEach((items) => {  //here passing 'items' parameter
+//     sum += items;
+// });
+
+// console.log(sum); // result 199
+
+
+
+
